43602e647c34fd324c6f2c79a0b97d3c440a8a86
LinhChi1502 <linhchidkc@gmail.com>
2020-12-16 12:01:31 +0700

    add

--- src/main/java/com/example/demo/controller/CityController.java
+++ src/main/java/com/example/demo/controller/CityController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -18,13 +19,13 @@ public class CityController {
     @Autowired
     private CityService cityService;
 
-   @GetMapping("")
+    @GetMapping("")
     public ModelAndView home() {
-       ModelAndView modelAndView = new ModelAndView("home");
-       Iterable<City> cities = cityService.findAll();
-       modelAndView.addObject("cities", cities);
-       return modelAndView;
-   }
+        ModelAndView modelAndView = new ModelAndView("home");
+        Iterable<City> cities = cityService.findAll();
+        modelAndView.addObject("cities", cities);
+        return modelAndView;
+    }
 
     @GetMapping("/detail/{id}")
     public ModelAndView detail(@PathVariable int id) {
@@ -44,12 +45,39 @@ public class CityController {
         return modelAndView;
     }
 
+    @PostMapping("/edit")
+    public ModelAndView edit(@ModelAttribute City city) {
+        ModelAndView modelAndView = new ModelAndView("home");
+        cityService.save(city);
+        Iterable<City> cities = cityService.findAll();
+        modelAndView.addObject("cities", cities);
+        return modelAndView;
+    }
+
     @GetMapping("/create")
     public ModelAndView create() {
         ModelAndView modelAndView = new ModelAndView("create");
         Iterable<Country> countries = countryService.findAll();
         modelAndView.addObject("city", new City());
         modelAndView.addObject("countries", countries);
+        return modelAndView;
+    }
+
+    @PostMapping("/create")
+    public ModelAndView create(@ModelAttribute City city) {
+        ModelAndView modelAndView = new ModelAndView("home");
+        cityService.save(city);
+        Iterable<City> cities = cityService.findAll();
+        modelAndView.addObject("cities", cities);
+        return modelAndView;
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView delete(@PathVariable int id) {
+        ModelAndView modelAndView = new ModelAndView("home");
+        cityService.remove(id);
+        Iterable<City> cities = cityService.findAll();
+        modelAndView.addObject("cities", cities);
         return modelAndView;
     }
 }
